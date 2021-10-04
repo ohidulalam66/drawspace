9fd391db56f3a6b0a347a6ecba456bd0b12c9af9
ohidulalam <mdnoyonctg15@gmail.com>
2021-10-04 20:23:23 +0600

    all set up without readme file

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0 me-3">
+                        <ul className="navbar-nav fw-bold ms-auto mb-2 mb-lg-0 me-3">
                             <li className="nav-item px-4">
                                 <Link className="nav-link" aria-current="page" to="/home">Home</Link>
                             </li>
